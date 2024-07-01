0a9f2897010d87bac9a09ace44cfd4049cfa6507
Carl Wallace <carl@redhoundsoftware.com>
2024-07-01 12:24:19 -0400

    refactor per suggestions from baloo

--- pittv3/src/sha1_sig.rs
+++ pittv3/src/sha1_sig.rs
@@ -1,13 +1,15 @@
 #![cfg(feature = "sha1_sig")]
 
-use certval::PathValidationStatus;
-use certval::PkiEnvironment;
+use certval::{PathValidationStatus, PkiEnvironment};
 use const_oid::db::rfc5912::SHA_1_WITH_RSA_ENCRYPTION;
-use der::Encode;
-use rsa::Pkcs1v15Sign;
-use rsa::RsaPublicKey;
-use sha1::{Digest, Sha1};
-use spki::{AlgorithmIdentifierOwned, DecodePublicKey, SubjectPublicKeyInfoOwned};
+use der::referenced::OwnedToRef;
+use rsa::{
+    pkcs1v15::{Signature, VerifyingKey},
+    signature::Verifier,
+    RsaPublicKey,
+};
+use sha1::Sha1;
+use spki::{AlgorithmIdentifierOwned, SubjectPublicKeyInfoOwned};
 
 /// Provides verify_signature_message implementation that targets RSA w/SHA1 only (in support of
 /// verifying certificates signed using RSA w/SHA1)
@@ -22,15 +24,7 @@ pub fn verify_signature_message_rust_crypto_sha1(
         return Err(certval::Error::Unrecognized);
     }
 
-    let enc_spki = match spki.to_der() {
-        Ok(enc_spki) => enc_spki,
-        Err(e) => {
-            println!("cargo::warning=Failed to encode public key passed to verify_signature_message_rust_crypto_sha1: {e:?}");
-            return Err(certval::Error::Asn1Error(e));
-        }
-    };
-
-    let rsa = match RsaPublicKey::from_public_key_der(&enc_spki) {
+    let rsa = match RsaPublicKey::try_from(spki.owned_to_ref()) {
         Ok(rsa) => rsa,
         Err(e) => {
             println!("cargo::warning=Failed to parse public key passed to verify_signature_message_rust_crypto_sha1 as an RSA public key: {e:?}");
@@ -38,9 +32,12 @@ pub fn verify_signature_message_rust_crypto_sha1(
         }
     };
 
-    let hash_to_verify = Sha1::digest(message_to_verify);
-    let ps = Pkcs1v15Sign::new::<Sha1>();
-    rsa.verify(ps, hash_to_verify.as_slice(), signature)
+    let verifying_key = VerifyingKey::<Sha1>::new(rsa);
+    verifying_key
+        .verify(
+            message_to_verify,
+            &Signature::try_from(signature).map_err(|_| certval::Error::ParseError)?,
+        )
         .map_err(|_err| {
             certval::Error::PathValidation(PathValidationStatus::SignatureVerificationFailure)
         })
