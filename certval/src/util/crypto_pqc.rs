ca771d956087f9aa748986293722b4b7fd460bdc
Carl Wallace <carl@redhoundsoftware.com>
2026-03-14 11:55:01 -0400

    add pre-hash SLH DSA support

--- certval/src/util/crypto_pqc.rs
+++ certval/src/util/crypto_pqc.rs
@@ -5,7 +5,8 @@
 use log::error;
 
 use ml_dsa::{MlDsa44, MlDsa65, MlDsa87};
-use sha2::{Digest, Sha512};
+use sha2::{Digest, Sha256, Sha512};
+use sha3::digest::{ExtendableOutput, Update, XofReader};
 use slh_dsa::signature::Verifier;
 
 use crate::{Error, PkiEnvironment};
@@ -27,7 +28,8 @@ macro_rules! pqverify_mldsa {
         match ml_dsa::Signature::<$pkt>::decode(&sig_bytes) {
             Some(sig) => match vk.verify($message_to_verify, &sig) {
                 Ok(_) => return Ok(()),
-                Err(_e) => {
+                Err(e) => {
+                    error!("Failed to verify ML DSA signature: {}", e);
                     return Err(Error::Unrecognized);
                 }
             },
@@ -62,6 +64,7 @@ macro_rules! pqverify_ph_mldsa {
                 match vk.verify_internal(&message_rep, &sig) {
                     true => return Ok(()),
                     false => {
+                        error!("Failed to verify Hash ML DSA signature");
                         return Err(Error::Unrecognized);
                     }
                 }
@@ -84,6 +87,68 @@ macro_rules! pqverify_slhdsa {
             .try_into()
             .map_err(|_e| Error::PqcValidation)?;
         match vk.verify($message_to_verify, &sig) {
+            Ok(_) => {
+                return Ok(());
+            }
+            Err(e) => {
+                error!("Failed to verify SLH DSA signature: {}", e);
+                return Err(Error::Unrecognized);
+            }
+        }
+    }};
+}
+
+macro_rules! pqverify_ph_slhdsa {
+    ($pkt:ty, $hash:ty, $oid:ident, $message_to_verify:ident, $spki_val:ident, $signature:ident) => {{
+        let vk = slh_dsa::VerifyingKey::<$pkt>::try_from($spki_val)
+            .map_err(|_e| Error::PqcValidation)?;
+        let sig: slh_dsa::Signature<$pkt> = $signature
+            .to_vec()
+            .as_slice()
+            .try_into()
+            .map_err(|_e| Error::PqcValidation)?;
+        let ph = <$hash>::digest($message_to_verify);
+        let one = [0x01];
+        let ctx_len = [0x00];
+        let mut message_rep = vec![];
+        message_rep.append(&mut one.to_vec());
+        message_rep.append(&mut ctx_len.to_vec());
+        message_rep.append(&mut $oid.to_vec());
+        message_rep.append(&mut ph.to_vec());
+        match vk.slh_verify_internal(&[&message_rep], &sig) {
+            Ok(_) => {
+                return Ok(());
+            }
+            Err(e) => {
+                error!("Failed to verify SLH DSA signature: {}", e);
+                return Err(Error::Unrecognized);
+            }
+        }
+    }};
+}
+
+macro_rules! pqverify_ph_slhdsa_shake {
+    ($pkt:ty, $shake:ty, $hash_len:expr, $oid:ident, $message_to_verify:ident, $spki_val:ident, $signature:ident) => {{
+        let vk = slh_dsa::VerifyingKey::<$pkt>::try_from($spki_val)
+            .map_err(|_e| Error::PqcValidation)?;
+        let sig: slh_dsa::Signature<$pkt> = $signature
+            .to_vec()
+            .as_slice()
+            .try_into()
+            .map_err(|_e| Error::PqcValidation)?;
+        let mut hasher = <$shake>::default();
+        hasher.update($message_to_verify);
+        let mut reader = hasher.finalize_xof();
+        let mut ph = [0u8; $hash_len];
+        reader.read(&mut ph);
+        let one = [0x01];
+        let ctx_len = [0x00];
+        let mut message_rep = vec![];
+        message_rep.append(&mut one.to_vec());
+        message_rep.append(&mut ctx_len.to_vec());
+        message_rep.append(&mut $oid.to_vec());
+        message_rep.append(&mut ph.to_vec());
+        match vk.slh_verify_internal(&[&message_rep], &sig) {
             Ok(_) => {
                 return Ok(());
             }
@@ -140,6 +205,42 @@ pub fn verify_signature_message_rustcrypto(
         pqverify_slhdsa!(slh_dsa::Shake256f, message_to_verify, spki_val, signature)
     } else if ID_SLH_DSA_SHAKE_256_S == signature_alg.oid {
         pqverify_slhdsa!(slh_dsa::Shake256s, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHA_2_128_S_WITH_SHA_256 == signature_alg.oid {
+        let sha256_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01];
+        pqverify_ph_slhdsa!(slh_dsa::Sha2_128s, Sha256, sha256_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHA_2_128_F_WITH_SHA_256 == signature_alg.oid {
+        let sha256_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01];
+        pqverify_ph_slhdsa!(slh_dsa::Sha2_128f, Sha256, sha256_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHA_2_192_S_WITH_SHA_512 == signature_alg.oid {
+        let sha512_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03];
+        pqverify_ph_slhdsa!(slh_dsa::Sha2_192s, Sha512, sha512_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHA_2_192_F_WITH_SHA_512 == signature_alg.oid {
+        let sha512_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03];
+        pqverify_ph_slhdsa!(slh_dsa::Sha2_192f, Sha512, sha512_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHA_2_256_S_WITH_SHA_512 == signature_alg.oid {
+        let sha512_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03];
+        pqverify_ph_slhdsa!(slh_dsa::Sha2_256s, Sha512, sha512_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHA_2_256_F_WITH_SHA_512 == signature_alg.oid {
+        let sha512_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03];
+        pqverify_ph_slhdsa!(slh_dsa::Sha2_256f, Sha512, sha512_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHAKE_128_S_WITH_SHAKE_128 == signature_alg.oid {
+        let shake128_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x0B];
+        pqverify_ph_slhdsa_shake!(slh_dsa::Shake128s, sha3::Shake128, 32, shake128_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHAKE_128_F_WITH_SHAKE_128 == signature_alg.oid {
+        let shake128_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x0B];
+        pqverify_ph_slhdsa_shake!(slh_dsa::Shake128f, sha3::Shake128, 32, shake128_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHAKE_192_S_WITH_SHAKE_256 == signature_alg.oid {
+        let shake256_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x0C];
+        pqverify_ph_slhdsa_shake!(slh_dsa::Shake192s, sha3::Shake256, 64, shake256_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHAKE_192_F_WITH_SHAKE_256 == signature_alg.oid {
+        let shake256_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x0C];
+        pqverify_ph_slhdsa_shake!(slh_dsa::Shake192f, sha3::Shake256, 64, shake256_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHAKE_256_S_WITH_SHAKE_256 == signature_alg.oid {
+        let shake256_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x0C];
+        pqverify_ph_slhdsa_shake!(slh_dsa::Shake256s, sha3::Shake256, 64, shake256_oid, message_to_verify, spki_val, signature)
+    } else if ID_HASH_SLH_DSA_SHAKE_256_F_WITH_SHAKE_256 == signature_alg.oid {
+        let shake256_oid = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x0C];
+        pqverify_ph_slhdsa_shake!(slh_dsa::Shake256f, sha3::Shake256, 64, shake256_oid, message_to_verify, spki_val, signature)
     }
     Err(Error::Unrecognized)
 }
